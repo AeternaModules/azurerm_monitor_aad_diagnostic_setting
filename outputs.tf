71994c7bf6b8de6f56f9aa466ea8e66c9a89b4c3
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:42:58 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "monitor_aad_diagnostic_settings" {
+  description = "All monitor_aad_diagnostic_setting resources"
+  value       = azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings
+}
+
---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "monitor_aad_diagnostic_settings" {
+  description = <<EOT
+Map of monitor_aad_diagnostic_settings, attributes below
+Required:
+    - name
+Optional:
+    - eventhub_authorization_rule_id
+    - eventhub_name
+    - log_analytics_workspace_id
+    - storage_account_id
+    - enabled_log (block):
+        - category (required)
+        - retention_policy (optional, block):
+            - days (optional)
+            - enabled (optional)
+EOT
+
+  type = map(object({
+    name                           = string
+    eventhub_authorization_rule_id = optional(string)
+    eventhub_name                  = optional(string)
+    log_analytics_workspace_id     = optional(string)
+    storage_account_id             = optional(string)
+    enabled_log = optional(object({
+      category = string
+      retention_policy = optional(object({
+        days    = optional(number, 0)
+        enabled = optional(bool, false)
+      }))
+    }))
+  }))
+}
+
