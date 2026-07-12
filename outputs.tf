34e06aa86e08e9088b71301fb1f4b39cded49f23
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "monitor_aad_diagnostic_settings" {
-  description = "All monitor_aad_diagnostic_setting resources"
-  value       = azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings
-}
 output "monitor_aad_diagnostic_settings_enabled_log" {
-  description = "List of enabled_log values across all monitor_aad_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : v.enabled_log]
+  description = "Map of enabled_log values across all monitor_aad_diagnostic_settings, keyed the same as var.monitor_aad_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : k => v.enabled_log }
 }
 output "monitor_aad_diagnostic_settings_eventhub_authorization_rule_id" {
-  description = "List of eventhub_authorization_rule_id values across all monitor_aad_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : v.eventhub_authorization_rule_id]
+  description = "Map of eventhub_authorization_rule_id values across all monitor_aad_diagnostic_settings, keyed the same as var.monitor_aad_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : k => v.eventhub_authorization_rule_id }
 }
 output "monitor_aad_diagnostic_settings_eventhub_name" {
-  description = "List of eventhub_name values across all monitor_aad_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : v.eventhub_name]
+  description = "Map of eventhub_name values across all monitor_aad_diagnostic_settings, keyed the same as var.monitor_aad_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : k => v.eventhub_name }
 }
 output "monitor_aad_diagnostic_settings_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all monitor_aad_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all monitor_aad_diagnostic_settings, keyed the same as var.monitor_aad_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : k => v.log_analytics_workspace_id }
 }
 output "monitor_aad_diagnostic_settings_name" {
-  description = "List of name values across all monitor_aad_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : v.name]
+  description = "Map of name values across all monitor_aad_diagnostic_settings, keyed the same as var.monitor_aad_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : k => v.name }
 }
 output "monitor_aad_diagnostic_settings_storage_account_id" {
-  description = "List of storage_account_id values across all monitor_aad_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : v.storage_account_id]
+  description = "Map of storage_account_id values across all monitor_aad_diagnostic_settings, keyed the same as var.monitor_aad_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_aad_diagnostic_setting.monitor_aad_diagnostic_settings : k => v.storage_account_id }
 }
 
